9902e725449ee29b44d9edaf05807e61d4cd0dd9
dothinh115 <dothinh115@gmail.com>
2022-12-12 21:13:05 +0700

    update

--- src/Hooks/useUpdateUser.js
+++ src/Hooks/useUpdateUser.js
@@ -22,7 +22,7 @@ const useUpdateUser = () => {
           dispatch(action);
         } catch (error) {
           console.log(error);
-          if(error.response.state === 401) {
+          if(error.response.status === 401) {
             localStorage.removeItem("loginInfo");
             window.location.reload(false);
           }
