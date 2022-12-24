61102a1e61aee0bf8f60d7a013fce90446f0d102
dothinh115 <dothinh115@gmail.com>
2022-12-25 00:46:33 +0700

    lazyload update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Index from './components/index/Index';
 import Register from './components/register/Register';
 import Login from './components/login/Login';
 import Detail from './components/detail/Detail';
-import Search from './components/search/Search';
 import Cart from './components/cart/Cart';
 import Profile from './components/profile/Profile';
 import Edit from './components/profile/Edit';
@@ -13,6 +12,7 @@ import { createBrowserHistory } from 'history';
 import { getToken } from './util/function';
 import LoggedInRoute from './hoc/NotLoggedInRoute';
 import NotLoggedInRoute from './hoc/LoggedInRoute';
+import React, { Suspense, lazy } from 'react';
 
 //npm i history => chuyển hướng trang ở file ko phải component
 export const history = createBrowserHistory();
@@ -22,21 +22,51 @@ function App() {
   let loggedIn = false;
   if (token) loggedIn = true;
 
+  const Search = lazy(() => import("./components/search/Search"));
+
   return (
     <HistoryRouter history={history}>
       <Routes>
         <Route path='/' element={<Homtemplate loggedIn={loggedIn} />} >
           <Route index element={<Index />} />
-          <Route path='/search' element={<Search />} />
-          <Route path='/detail/:productId' element={<Detail />} />
+          <Route path='/search' element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Search />
+            </Suspense>
+          } />
+          <Route path='/detail/:productId' element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Detail />
+            </Suspense>
+          } />
           <Route element={<NotLoggedInRoute loggedIn={loggedIn} />} >
-            <Route path="/register" element={<Register />} />
-            <Route path='/login' element={<Login />} />
+            <Route path="/register" element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Register />
+              </Suspense>
+            } />
+            <Route path='/login' element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Login />
+              </Suspense>
+            } />
           </Route>
           <Route element={<LoggedInRoute loggedIn={loggedIn} />}>
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/profile' element={<Profile />}>
-              <Route path='/profile/edit' element={<Edit />} />
+            <Route path='/cart' element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Cart />
+              </Suspense>
+            } />
+            <Route path='/profile' element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Profile />
+              </Suspense>
+            }>
+              <Route path='/profile/edit' element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Edit />
+                </Suspense>
+              } />
             </Route>
           </Route>
           <Route path='*' element={<Navigate to='/' />} />
