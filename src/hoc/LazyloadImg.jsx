ff4cb3ce289cbb53936a364af41b9c7ac2f03d32
dothinh115 <dothinh115@gmail.com>
2022-12-25 06:28:44 +0700

    img lazyload update

--- src/hoc/LazyloadImg.jsx
+++ src/hoc/LazyloadImg.jsx
@@ -3,7 +3,7 @@ import LazyLoad from 'react-lazyload'
 
 const LazyloadImg = ({url}) => {
   return (
-    <LazyLoad throttle={500} offset={100} placeholder={<div style={{height: "300px", display: "flex", alignItems: "center"}}><div className="loader"></div></div>}>
+    <LazyLoad throttle={200} offset={100} placeholder={<div style={{height: "300px", display: "flex", alignItems: "center"}}><div className="loader"></div></div>}>
         <img src={url} alt="" />
     </LazyLoad>
   )
