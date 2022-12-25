6e0f32fca40f29b35146f313ab02359c961c3269
dothinh115 <dothinh115@gmail.com>
2022-12-26 00:09:11 +0700

    performance improve

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -53,7 +53,7 @@ const Register = () => {
     setError({
       ...error,
       [id]: errMess
-    });  
+    });
     setDataValue({
       ...dataValue,
       [id]: value
@@ -104,70 +104,74 @@ const Register = () => {
           </h1>
         </div>
         <div className="main-body">
-          {result.result 
-          ? <><i className="fa-solid fa-check" style={{ color: "green" }}></i>Đăng ký thành công, {<Link className="alert-link" to="/login"><i className="fa-solid fa-arrow-right"></i>bấm vào đây</Link>} để đăng nhập!!</>
-          : <><i className="fa-solid fa-circle-exclamation" style={{ color: "red" }}></i> {result.message}</>
+          {result.result
+            ? <><i className="fa-solid fa-check" style={{ color: "green" }}></i>Đăng ký thành công, {<Link className="alert-link" to="/login"><i className="fa-solid fa-arrow-right"></i>bấm vào đây</Link>} để đăng nhập!!</>
+            : <><i className="fa-solid fa-circle-exclamation" style={{ color: "red" }}></i> {result.message}</>
           }
         </div>
       </div>}
-      <div className="main-container" style={{ marginTop: result.message && "20px" }}>
-        <div className="page-header">
-          <h1>
-            ĐĂNG KÝ TÀI KHOẢN
-          </h1>
-        </div>
-        <div className="main-body">
-          <div className="register-form">
-            <form onSubmit={e => submitHandle(e)}>
-              <div className="register-form-main">
-                {dataConfig.id.map((item, index) => {
-                  return (
-                    <div key={index} className="item">
-                      <div className="item-left">
-                        {<i className={`fa-solid fa-${dataConfig.icon[index]}`}></i>}
-                        {dataConfig.name[index]}
-                      </div>
-                      <div className="item-right">
-                        {item === "gender" ? <select name="gender" data-id={item} onChange={e => inputChangeHandle(e)} value={dataValue.gender}>
-                          <option value="true">
-                            Nam
-                          </option>
-                          <option value="false">
-                            Nữ
-                          </option>
-                        </select> : <input
-                          data-id={item}
-                          type={item === "password" ? "password" : "text"}
-                          onChange={e => inputChangeHandle(e)}
-                          className={`${error[item] && "isInvalid"} ${!error[item] && dataValue[item] && "isValid"}`}
-                          placeholder={dataConfig.placeHolder[index]}
-                          defaultValue={dataValue[item]}
-                        />}
-                        {error[item] && <div className="form-error">
-                          <i className="fa-solid fa-circle-exclamation" style={{ color: "red" }}></i>
-                          {error[item]}
-                        </div>}
-                      </div>
-                    </div>
-                  )
-                })}
-                <div className="item">
-                  <div className="item-left">
 
-                  </div>
-                  <div className="item-right">
-                    <div className="form-button">
-                      <button className="btn" disabled={valid ? false : true} onClick={e => regButtonHandle(e)}>
-                        Đăng ký
-                      </button>
+      {!result.result &&
+        <div className="main-container" style={{ marginTop: result.message && "20px" }}>
+          <div className="page-header">
+            <h1>
+              ĐĂNG KÝ TÀI KHOẢN
+            </h1>
+          </div>
+          <div className="main-body">
+            <div className="register-form">
+              <form onSubmit={e => submitHandle(e)}>
+                <div className="register-form-main">
+                  {dataConfig.id.map((item, index) => {
+                    return (
+                      <div key={index} className="item">
+                        <div className="item-left">
+                          {<i className={`fa-solid fa-${dataConfig.icon[index]}`}></i>}
+                          {dataConfig.name[index]}
+                        </div>
+                        <div className="item-right">
+                          {item === "gender" ? <select name="gender" data-id={item} onChange={e => inputChangeHandle(e)} value={dataValue.gender}>
+                            <option value="true">
+                              Nam
+                            </option>
+                            <option value="false">
+                              Nữ
+                            </option>
+                          </select> : <input
+                            data-id={item}
+                            type={item === "password" ? "password" : "text"}
+                            onChange={e => inputChangeHandle(e)}
+                            className={`${error[item] && "isInvalid"} ${!error[item] && dataValue[item] && "isValid"}`}
+                            placeholder={dataConfig.placeHolder[index]}
+                            defaultValue={dataValue[item]}
+                          />}
+                          {error[item] && <div className="form-error">
+                            <i className="fa-solid fa-circle-exclamation" style={{ color: "red" }}></i>
+                            {error[item]}
+                          </div>}
+                        </div>
+                      </div>
+                    )
+                  })}
+                  <div className="item">
+                    <div className="item-left">
+
+                    </div>
+                    <div className="item-right">
+                      <div className="form-button">
+                        <button className="btn" disabled={valid ? false : true} onClick={e => regButtonHandle(e)}>
+                          Đăng ký
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      }
+
     </>
   )
 }
