d41bace380dfaa01c74193cd3c9a76d3eaa6f0b0
dothinh115 <dothinh115@gmail.com>
2022-12-25 23:37:19 +0700

    performance improve

--- src/components/profile/OrderHistory.jsx
+++ src/components/profile/OrderHistory.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { getProfileApi } from '../../redux/reducers/userReducer';
@@ -24,6 +24,10 @@ const OrderHistory = () => {
     e.preventDefault();
     setSeeAll(seeAll + 3);
   }
+
+  useEffect(() => {
+    setDeleting(null);
+  }, [userData?.ordersHistory]);
 
   return (
     <>
